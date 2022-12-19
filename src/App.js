bf7e3522b88a8a251cbf7781fd45dd1e000c39ef
woliart <artwol993@gmail.com>
2022-12-19 21:41:33 +0100

    add main structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+
+import Header from './component/header';
+import Footer from './component/footer';
+import Body from './component/body';
+
 import './style/App.css';
+
+
 
 function App() {
   return (
     <div className="App">
      <p>Rent Car</p>
+     <Header />
+     <Body />
+     <Footer />
     </div>
   );
 }
