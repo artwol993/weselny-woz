ea1630aaf163f3da9d2445457005073341a3d55c
woliart <artwol993@gmail.com>
2023-01-10 21:52:27 +0100

    add responsive navbar

--- src/component/header.js
+++ src/component/header.js
@@ -9,9 +9,11 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 function Header() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">Rent Car</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
           <Nav.Link href="#home">Witaj</Nav.Link>
           <Nav.Link href="#about-us">O nas</Nav.Link>
@@ -19,12 +21,14 @@ function Header() {
           <Nav.Link href="#cars">Samochody</Nav.Link>
           <Nav.Link href="#news">Aktualności</Nav.Link>
           <Nav.Link href="#contact">Kontakt</Nav.Link>
-            <NavDropdown title="Język" id="nav-dropdown" variant="dark">
+            <NavDropdown title="Język" id="nav-dropdown" bg="dark" variant="dark">
               <NavDropdown.Item eventKey="1.1">PL</NavDropdown.Item>
               <NavDropdown.Item eventKey="1.2">EN</NavDropdown.Item>
               <NavDropdown.Item eventKey="1.3">UA</NavDropdown.Item>
             </NavDropdown>
         </Nav>
+        </Navbar.Collapse>
+
     </Container>
     </Navbar>
     </>
