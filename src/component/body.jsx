04863debe91ba3cd73e8088d7ce3422bb57cd021
woliart <artwol993@gmail.com>
2023-01-31 20:57:47 +0100

    add basic files

--- src/component/body.jsx
+++ src/component/body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutUs from './about-us'
 import WelcomeCarousel from './welcome-carousel'
+import Offer from './offer'
 
 function Body() {
   return (
     <div>
       <WelcomeCarousel />
       <AboutUs />
+      <Offer />
     </div>
   )
 }
