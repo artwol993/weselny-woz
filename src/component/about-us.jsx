d8e50239a04c3f430c7e2ea3a52ff5839cc638fe
woliart <artwol993@gmail.com>
2023-01-31 21:16:57 +0100

    add basic files about-us

---
+++ src/component/about-us.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "../style/about-us.css"
+
+function AboutUs() {
+  return (
+    <Container p-5 >
+      <div className="container-about-us">
+        <p >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi alias quod id eaque tenetur! Quis reiciendis voluptate, magnam delectus possimus hic dolores praesentium eaque aperiam in corrupti dicta ducimus voluptatum officia veritatis earum debitis exercitationem perspiciatis numquam distinctio, ipsa dolore ratione doloremque? Doloremque vitae, illo dicta est veritatis magnam. Modi architecto earum eaque sequi obcaecati voluptatem, natus optio! Nihil unde beatae harum sed ad, temporibus officia nesciunt labore suscipit cumque earum nulla maiores amet aliquam quidem possimus, quo delectus? Illo eaque quidem obcaecati expedita dolore distinctio consectetur sapiente nobis voluptatibus iusto culpa, recusandae odit! Ipsa itaque obcaecati iste vero doloremque.
+        </p>
+      </div>
+    </Container>
+  )
+}
+
+export default AboutUs
