049dd7d7b04e8fe708c0167aa4be5b62c817a696
woliart <artwol993@gmail.com>
2023-01-31 20:48:56 +0100

    add basic responsive about-us

--- src/component/body.jsx
+++ src/component/body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AboutUs from './about-us'
 import WelcomeCarousel from './welcome-carousel'
 
 function Body() {
   return (
     <div>
       <WelcomeCarousel />
+      <AboutUs />
     </div>
   )
 }
