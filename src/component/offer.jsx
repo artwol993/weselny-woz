28989b6ea8229469862df4a1b60c5b4fc7517489
woliart <artwol993@gmail.com>
2023-01-31 22:26:10 +0100

    add basic offer section

--- src/component/offer.jsx
+++ src/component/offer.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 function Offer() {
   return (
-    <div>Offer</div>
-  )
+    <CardGroup className='m-5'>
+    <Card border="info" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card border="info" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="../assets/2.svg" />
+      <Card.Body>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This card has supporting text below as a natural lead-in to
+          additional content.{' '}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card border="info" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Body>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This card has even longer content than the
+          first to show that equal height action.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </CardGroup>
+);
 }
 
 export default Offer
