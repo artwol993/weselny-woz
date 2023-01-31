eee08ff5a1df9981b00f2060dcc4273d029c1a7a
Artur <61992974+artwol993@users.noreply.github.com>
2023-01-31 21:18:11 +0100

    Revert "add basic offer files"

--- src/component/body.jsx
+++ src/component/body.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import AboutUs from './about-us'
 import WelcomeCarousel from './welcome-carousel'
-import Offer from './offer'
 
 function Body() {
   return (
     <div>
       <WelcomeCarousel />
       <AboutUs />
-      <Offer />
     </div>
   )
 }
