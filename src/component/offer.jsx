b7b6e99a3155b6a3cc13b07236a5f9bf023886ef
woliart <artwol993@gmail.com>
2023-01-31 22:32:24 +0100

    update images

--- src/component/offer.jsx
+++ src/component/offer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import "../assets/2.jpg"
 
 function Offer() {
   return (
     <CardGroup className='m-5'>
     <Card border="info" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Img variant="top" src="https://thenounproject.com/api/private/icons/5469157/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABj2YX4FjBYLLHqU9dQmqENFg2QUliRkGFev5AY6Ty4SCCawu_JbWC42hMzrJU3IfJBp6_jDn2VTqBOL26DOjGDq20kAw%3D%3D" />
       <Card.Body>
         <Card.Title>Card title</Card.Title>
         <Card.Text>
@@ -16,7 +17,7 @@ function Offer() {
       </Card.Body>
     </Card>
     <Card border="info" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="../assets/2.svg" />
+      <Card.Img variant="top" src="https://static.thenounproject.com/attribution/416360-600.png" />
       <Card.Body>
         <Card.Title>Card title</Card.Title>
         <Card.Text>
@@ -26,7 +27,7 @@ function Offer() {
       </Card.Body>
     </Card>
     <Card border="info" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px160" />
+      <Card.Img variant="top" src="https://thenounproject.com/api/private/icons/421805/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABj2YX4FjBYLLHqU9dQmqENFg2QUliRkGFev5AY6Ty4SCCawu_JbWC42hMzrJU3IfJBp6_jDn2VTqBOL26DOjGDq20kAw%3D%3D" />
       <Card.Body>
         <Card.Title>Card title</Card.Title>
         <Card.Text>
