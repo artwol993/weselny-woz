e51c597ed56e31bc7d600bbc881b3f740daa9011
woliart <artwol993@gmail.com>
2023-02-11 12:00:00 +0100

    add style

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,24 +2,40 @@ import React from "react";
 import "../style/footer.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { Col } from "react-bootstrap";
-// import phone from "../assets/phone.png";
+import Col from "react-bootstrap/Col";
+import phone from "../assets/phone.png";
+import intstagram from "../assets/instagram.png";
+import facebook from "../assets/facebook.png";
 
 function Footer() {
   return (
     <>
       <section className="footer">
-        <Container>
-          <h3>Skontaktuj się z nami</h3>
+        <Container fluid>
+          <p className="h1 mb-5">Skontaktuj się z nami</p>
           <Row>
             <Col>
-              <p>555666333</p>
+              <a href="555666333">
+                <div>
+                  <p className="h3 text-white">555666333</p>
+                </div>
+              </a>
             </Col>
-            <Col>biuro@rent-car.pl</Col>
+            <Col>
+              <a href="mailto:biuro@rent-car.pl">
+                <p className="h3 text-white"> biuro@rent-car.pl</p>
+              </a>
+            </Col>
           </Row>
           <Row>
-            <p>fb</p>
-            <p>int</p>
+            <Container>
+              <div>
+                <img src={intstagram} alt={intstagram}></img>
+                <a href="http://www.instagram.com"></a>
+                <img src={facebook} alt={facebook}></img>
+                <a href="http://www.facebook.com"></a>
+              </div>
+            </Container>
           </Row>
         </Container>
       </section>
