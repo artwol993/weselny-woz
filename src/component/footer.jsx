66fa90116402653c301a8ddb40791a5c00b0827f
woliart <artwol993@gmail.com>
2023-02-11 11:12:31 +0100

    add basic structure

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "../style/footer.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { Col } from "react-bootstrap";
+// import phone from "../assets/phone.png";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <>
+      <section className="footer">
+        <Container>
+          <h3>Skontaktuj się z nami</h3>
+          <Row>
+            <Col>
+              <p>555666333</p>
+            </Col>
+            <Col>biuro@rent-car.pl</Col>
+          </Row>
+          <Row>
+            <p>fb</p>
+            <p>int</p>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
 
 export default Footer;
