da5c868d1f857e1128a1c38a2d8f4fe54bf0fc88
woliart <artwol993@gmail.com>
2023-03-11 16:31:34 +0100

    resolve footer bug

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "../style/footer.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import intstagram from "../assets/instagram.png";
+import facebook from "../assets/facebook.png";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <section className="footer">
+        <Container fluid>
+          <p className="h1 mb-5">Skontaktuj się z nami</p>
+          <Row>
+            <Col>
+              <a href="555666333">
+                <div>
+                  <p className="h3 text-white">555666333</p>
+                </div>
+              </a>
+            </Col>
+            <Col>
+              <a href="biuro@rent-car.pl">
+                <p className="h3 text-white"> biuro@rent-car.pl</p>
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Container>
+              <div>
+                <img src={intstagram} alt={intstagram}></img>
+                <a href="http://www.instagram.com"> </a>
+                <img src={facebook} alt={facebook}></img>
+                <a href="http://www.facebook.com"> </a>
+              </div>
+            </Container>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
