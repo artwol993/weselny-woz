e836ae8321eccaf620bc2339ba013224e3b0e15f
Artur <artwol993@gmail.com>
2023-03-19 21:21:08 +0100

    add api to image carousel

--- src/component/welcome-carousel.jsx
+++ src/component/welcome-carousel.jsx
@@ -1,40 +1,50 @@
-import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import "../style/welcome-carousel.css";
-import wc1 from "../assets/wc1.jpg";
-import wc2 from "../assets/wc2.jpg";
-import wc3 from "../assets/wc3.png"
+import Cars from "./cars-data";
 
 function WelcomeCarousel() {
-  return (
-    <section id ="welcome">
-    <Carousel variant="dark" fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 wc-image"
-          src={wc1}
-          alt="First slide"
-          style={{ maxHeight: '800px' }}
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 wc-image"
-          src={wc2}
-          alt="Second slide"
-          style={{ maxHeight: '800px' }}
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 wc-image"
-          src={wc3}
-          alt="Third slide"
-          style={{ maxHeight: '800px' }}
-        />
-      </Carousel.Item>
-    </Carousel>
+  const [items, setItems] = useState(Cars);
+
+return (
+  <>
+    <section id="cars" className="container-cars">
+     
+        {Array.from({ length: 1 }).map(() =>
+          items.map((elem) => {
+            const { id, name, image } = elem;
+
+            return (
+              <Carousel variant="dark" fade>
+              <Carousel.Item>
+               <img
+                 className="d-block w-100 wc-image"
+                 src={image}
+                 alt={name}
+                 style={{ maxHeight: "800px" }} />
+             </Carousel.Item>
+             <Carousel.Item>
+               <img
+                 className="d-block w-100 wc-image"
+                 src={image}
+                 alt={name}
+                 style={{ maxHeight: "800px" }} />
+             </Carousel.Item>
+             <Carousel.Item>
+               <img
+                   className="d-block w-100 wc-image"
+                   src={image}
+                   alt={name}
+                   style={{ maxHeight: "800px" }} />
+               </Carousel.Item>
+              </Carousel>
+            );
+          })
+        )}
+
     </section>
-  );
+  </>
+);
 }
 
 export default WelcomeCarousel;
