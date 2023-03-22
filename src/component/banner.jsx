2073e2873919cb6b4060cf2ce978b4178f9f6ffc
Artur <artwol993@gmail.com>
2023-03-22 22:54:30 +0100

    add content

--- src/component/banner.jsx
+++ src/component/banner.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "../style/banner.css";
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <section id="banner">
+      <div className="banner-container">
+        <p className="banner-p">Sprawdź nasze promocje</p>
+      </div>
+    </section>
+  );
 }
 
 export default Banner;
