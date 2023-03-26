323de7d57ef7b6baef830682a22bede087fdba47
Artur <artwol993@gmail.com>
2023-03-26 12:47:25 +0200

    add svg

--- src/component/instruction.jsx
+++ src/component/instruction.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../style/instruction.css";
+import { Container } from "react-bootstrap";
+import { Rings } from "../assets/images/rings";
 
 function Instruction() {
-  return <div>Instruction</div>;
+  return (
+    <>
+      <section
+        id="instruction"
+        className="section-instruction"
+        style={{ backgroundImage: `url(${Rings})` }}
+      >
+        <Container>
+          <div>
+            <p>krok pierwszy</p>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 }
 
 export default Instruction;
