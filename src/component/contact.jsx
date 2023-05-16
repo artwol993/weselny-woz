545c1e2b4ce14dcc8684cab65dace15e03b31785
Artur <artwol993@gmail.com>
2023-05-16 21:23:55 +0200

    repair zip-code label

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -31,30 +31,29 @@ function Contact() {
                     <Form.Control type="date" />
                   </Form.Group>
                 </Row>
-
                 <Row className="mb-3">
                   <Form.Group as={Col} controlId="city-name">
                     <FloatingLabel controlId="city" label="Miejscowość">
                       <Form.Control type="city" placeholder="Warszawa" />
                     </FloatingLabel>
                   </Form.Group>
-
                   <Form.Group as={Col} controlId="city-zip">
-                    <FloatingLabel as={Col} controlId="formZip">
-                      <Form.Label>Kod pocztowy</Form.Label>
-                      <Form.Control />
+                    <FloatingLabel controlId="formZip" label="Kod pocztowy">
+                      <Form.Control type="city-zip" placeholder="01-234" />
                     </FloatingLabel>
                   </Form.Group>
                 </Row>
-
                 <Form.Group as={Col} controlId="user-name" className="mb-3">
                   <FloatingLabel controlId="floatingInput" label="Imię">
-                    <Form.Control type="name" placeholder="name" />
+                    <Form.Control type="name" placeholder="Jan" />
                   </FloatingLabel>
                 </Form.Group>
                 <Form.Group as={Col} controlId="user-email" className="mb-3">
                   <FloatingLabel controlId="floatingInput" label="Email">
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control
+                      type="email"
+                      placeholder="jan@weselny-woz.com"
+                    />
                   </FloatingLabel>
                 </Form.Group>
                 <Form.Group as={Col} controlId="user-phone" className="mb-3">
@@ -62,7 +61,6 @@ function Contact() {
                     <Form.Control type="phone" placeholder="+48 111222333" />
                   </FloatingLabel>
                 </Form.Group>
-
                 <FloatingLabel
                   controlId="floatingTextarea2"
                   label="Dodatkowe informacje"
