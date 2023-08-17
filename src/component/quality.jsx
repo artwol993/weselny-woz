82b5fb34a9f2ebeb3114a4f3549141d832c52417
Artur <artwol993@gmail.com>
2023-08-17 21:55:56 +0200

    rename

--- src/component/quality.jsx
+++ src/component/quality.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import "../style/offer.css";
-import offerCar from "../assets/offerCar.png";
-import offerTeam from "../assets/offerTeam.png";
-import offerProffesional from "../assets/offerProffesional.png";
+import "../style/quality.css";
+import qualityCar from "../assets/qualityCar.png";
+import qualityTeam from "../assets/qualityTeam.png";
+import qualityProffesional from "../assets/qualityProffesional.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
-function Offer() {
+function Quality() {
   return (
-    <section id="about-us" className="offer-container p-5">
+    <section id="Quality" className="quality-container p-5">
       <Container>
         <Row className="text-center g-4">
           <Col md>
             <div className=" text-white adv-card">
               <Card.Body className=" text-center d-flex flex-column justify-content-center align-items-center">
                 <div className="mb-2 box-circle">
-                  <img className="offer-icon" src={offerCar} alt="dos" />
+                  <img className="quality-icon" src={qualityCar} alt="dos" />
                 </div>
                 <Card.Title className=" mb-3">Flota</Card.Title>
                 <Card.Text>
@@ -33,7 +33,7 @@ function Offer() {
             <div className=" text-white adv-card">
               <Card.Body className=" text-center d-flex flex-column justify-content-center align-items-center">
                 <div className="mb-2 box-circle">
-                  <img className="offer-icon" src={offerTeam} alt="dos" />
+                  <img className="quality-icon" src={qualityTeam} alt="dos" />
                 </div>
                 <Card.Title className=" mb-3">Zespół</Card.Title>
                 <Card.Text>
@@ -50,8 +50,8 @@ function Offer() {
               <Card.Body className=" text-center d-flex flex-column justify-content-center align-items-center">
                 <div className="mb-2 box-circle">
                   <img
-                    className="offer-icon"
-                    src={offerProffesional}
+                    className="quality-icon"
+                    src={qualityProffesional}
                     alt="dos"
                   />
                 </div>
@@ -71,4 +71,4 @@ function Offer() {
   );
 }
 
-export default Offer;
+export default Quality;
