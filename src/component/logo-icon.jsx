7f61be7bc0765f27ba0c84b976e763c156293a95
Artur <artwol993@gmail.com>
2023-08-20 00:14:38 +0200

    update size

--- src/component/logo-icon.jsx
+++ src/component/logo-icon.jsx
@@ -5,7 +5,7 @@ function LogoIcon(props) {
     <svg
       version="1.0"
       xmlns="http://www.w3.org/2000/svg"
-      width="50px"
+      width="60px"
       viewBox="0 0 719 497"
       fill="#CBB26B"
       preserveAspectRatio="xMidYMid meet"
