69a1d97001d0b6e25dfb69e9d7575f6e5be6e7ee
Artur <artwol993@gmail.com>
2023-08-19 16:43:30 +0200

    update text

--- src/component/terms.jsx
+++ src/component/terms.jsx
@@ -10,35 +10,33 @@ function Terms() {
           Podstawowe warunki usługi
         </h2>
         <div className="terms-item">
-          <i className="fas fa-route terms-icon"></i>
           <p>
-            Do 100 km od Łukowa wliczone w cenę. Dystans powyżej 100 km podlega
-            indywidualnej wycenie.
+            <b>100 km od Łukowa w cenie!</b> Zacznij swoją podróż bez
+            dodatkowych kosztów.
           </p>
         </div>
         <div className="terms-item">
-          <i className="fas fa-clock terms-icon"></i>
           <p>
-            Pierwsze 5 godzin w cenie; za każdą kolejną obowiązuje dodatkowa
-            opłata.
+            <b>Pierwsze 5 godzin gratis</b> Potrzebujesz więcej czasu?
+            Zaproponujemy Ci korzystną ofertę.
           </p>
         </div>
         <div className="terms-item">
-          <i className="fas fa-user-tie terms-icon"></i>
           <p>
-            Samochodem zawsze kieruje wyznaczony przez nas profesjonalny
-            kierowca.
+            <b>Profesjonalizm za kierownicą</b> Za kierownicą zawsze znajduje
+            się jeden z naszych doświadczonych kierowców.
           </p>
         </div>
         <div className="terms-item">
-          <i className="fas fa-map-marker-alt terms-icon"></i>
-          <p>Oferujemy usługi w promieniu do 150 km od Łukowa.</p>
+          <p>
+            <b>Elastyczny zasięg</b> Obsługujemy tereny w promieniu aż 300 km od
+            Łukowa.
+          </p>
         </div>
         <div className="terms-item">
-          <i className="fas fa-car-side terms-icon"></i>
           <p>
-            Oferujemy możliwość eleganckiego udekorowania auta naszymi
-            ekskluzywnymi ozdobami.
+            <b>Personalizacja</b> Chcesz unikalne dekoracje? Dostosujemy auto do
+            Twojego stylu.
           </p>
         </div>
       </Container>
