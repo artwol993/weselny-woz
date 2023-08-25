e0ea210bd4f2822f6aa3e697c3d59a1e02bf5541
Artur <artwol993@gmail.com>
2023-08-25 17:32:22 +0200

    add favico

--- src/component/terms.jsx
+++ src/component/terms.jsx
@@ -10,26 +10,26 @@ function Terms() {
           Podstawowe warunki usługi
         </h2>
         <div className="terms-item">
-          <p class="h5">100 km od Łukowa w cenie!</p>
+          <p class="terms-item-title">100 km od Łukowa w cenie!</p>
           <p>Zacznij swoją podróż bez dodatkowych kosztów.</p>
         </div>
         <div className="terms-item">
-          <p class="h5">Pierwsze 5 godzin gratis</p>
+          <p class="terms-item-title">Pierwsze 5 godzin gratis</p>
           <p>Potrzebujesz więcej czasu? Zaproponujemy Ci korzystną ofertę.</p>
         </div>
         <div className="terms-item">
-          <p class="h5">Profesjonalizm za kierownicą</p>
+          <p class="terms-item-title">Profesjonalizm za kierownicą</p>
           <p>
             Za kierownicą zawsze znajduje się jeden z naszych doświadczonych
             kierowców.
           </p>
         </div>
         <div className="terms-item">
-          <p class="h5">Elastyczny zasięg</p>
+          <p class="terms-item-title">Elastyczny zasięg</p>
           <p> Obsługujemy tereny w promieniu aż 150 km od Łukowa.</p>
         </div>
         <div className="terms-item">
-          <p class="h5">Personalizacja</p>
+          <p class="terms-item-title">Personalizacja</p>
           <p>Chcesz unikalne dekoracje? Dostosujemy auto do Twojego stylu.</p>
         </div>
       </Container>
