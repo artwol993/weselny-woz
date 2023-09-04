d26f49f16f5ac0df78cd2231790b9af3994ee34f
Artur <artwol993@gmail.com>
2023-09-04 20:36:05 +0200

    add fies

--- src/component/gallery-data.jsx
+++ src/component/gallery-data.jsx
@@ -3,7 +3,7 @@ const Cars = [
     id: 1,
     image: "images/s-1.jpg",
     name: "Mercedes S",
-    category: "sport",
+    category: "limousine",
     price: "1100pln",
     gallery: ["images/s-2.jpg", "images/s-3.jpg"],
     description:
@@ -13,9 +13,9 @@ const Cars = [
     id: 2,
     image: "images/p-1.jpg",
     name: "Porsche",
-    category: "classic",
+    category: "sport",
     price: "1200pln",
-    gallery: ["images/p-2.jpg", "images/p-3.jpg"],
+    gallery: ["images/p-2.jpg", "images/p-3.jpg", "images/p-4.jpg"],
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, voluptatibus? Nemo tenetur quam, tempore magnam molestiae voluptatibus beatae aspernatur cum.",
   },
@@ -25,25 +25,35 @@ const Cars = [
     name: "Audi A6",
     category: "limousine",
     price: "1300pln",
-    gallery: ["images/a6-1.jpg", "images/16-2.jpg", "images/a6-3.jpg"],
+    gallery: ["images/a6-3.jpg"],
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, voluptatibus? Nemo tenetur quam, tempore magnam molestiae voluptatibus beatae aspernatur cum.",
   },
   {
     id: 4,
-    image: "images/car4.jpg",
-    name: "car4",
+    image: "images/aq8-1.jpg",
+    name: "Audi Q8 S-line",
     price: "1400pln",
-    gallery: ["images/car1.1.jpg", "images/car1.2.jpg", "images/car1.3.jpg"],
+    gallery: [
+      "images/aq8-2.jpg",
+      "images/aq8-3.jpg",
+      "images/aq8-4.jpg",
+      "images/aq8-5.jpg",
+    ],
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, voluptatibus? Nemo tenetur quam, tempore magnam molestiae voluptatibus beatae aspernatur cum.",
   },
   {
     id: 5,
-    image: "images/car5.jpg",
-    name: "car5",
+    image: "images/rs6-1.jpg",
+    name: "Audi RS6",
     price: "1500pln",
-    gallery: ["images/car1.1.jpg", "images/car1.2.jpg", "images/car1.3.jpg"],
+    gallery: [
+      "images/rs6-2.jpg",
+      "images/rs6-3.jpg",
+      "images/rs6-4.jpg",
+      "images/rs6-5.jpg",
+    ],
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, voluptatibus? Nemo tenetur quam, tempore magnam molestiae voluptatibus beatae aspernatur cum.",
   },
