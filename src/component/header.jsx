045ccdd9d51c0007dc14c83f8a851d2e40b55873
Artur <artwol993@gmail.com>
2023-09-14 00:17:19 +0200

    update header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,42 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/header.css";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Logo from "./logo";
+import SmallLogo from "./small-logo";
 
 function Header() {
+  const [isHeaderReduced, setIsHeaderReduced] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setIsHeaderReduced(true);
+      } else {
+        setIsHeaderReduced(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className="sticky-header">
-        <Navbar expand="lg" className="navbar-dark" aria-label="Header">
-          <Container className="d-flex justify-content-between">
-            <Navbar.Brand href="#home">
-              <Logo />
-            </Navbar.Brand>
-            <Navbar.Toggle as="button" className="navbar-toggler-custom" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-                <Nav.Link key="home" href="#home">
-                  Witaj
-                </Nav.Link>
-                <Nav.Link key="about-us" href="#about-us">
-                  O nas
-                </Nav.Link>
-                <Nav.Link key="terms" href="#terms">
-                  Oferta
-                </Nav.Link>
-                <Nav.Link key="gallery" href="#gallery">
-                  Nasze Samochody
-                </Nav.Link>
-                <Nav.Link key="contact" href="#contact">
-                  Kontakt
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
+      <Navbar expand="lg" className="navbar-dark" aria-label="Header">
+        <Container className="d-flex justify-content-between">
+          <Navbar.Brand href="#home">
+            {isHeaderReduced ? <SmallLogo className="small-logo" /> : <Logo />}
+          </Navbar.Brand>
+          <Navbar.Toggle as="button" className="navbar-toggler-custom" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link key="home" href="#home">
+                Witaj
+              </Nav.Link>
+              <Nav.Link key="about-us" href="#about-us">
+                O nas
+              </Nav.Link>
+              <Nav.Link key="terms" href="#terms">
+                Oferta
+              </Nav.Link>
+              <Nav.Link key="gallery" href="#gallery">
+                Nasze Samochody
+              </Nav.Link>
+              <Nav.Link key="contact" href="#contact">
+                Kontakt
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
---
+++ src/component/small-logo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../style/small-logo.css";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import weselnyWozIcon from "../assets/weselnyWozIcon.png";
+
+function SmallLogo() {
+  return (
+    <>
+      <Container>
+        <Card.Body className=" text-center d-flex justify-content-center align-items-center">
+          <div className="box-sm d-flex flex-column justify-content-center align-items-center">
+            <img className="logo-icon-sm" src={weselnyWozIcon} alt="dos" />
+            <div className="logo-tittle-sm">WESELNY WÓZ</div>
+          </div>
+        </Card.Body>
+      </Container>
+    </>
+  );
+}
+
+export default SmallLogo;
