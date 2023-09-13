160c45372b85ecaa6cda15bc9040db3111717c16
Artur <artwol993@gmail.com>
2023-09-13 22:14:02 +0200

    add stick to header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -8,23 +8,35 @@ import Logo from "./logo";
 function Header() {
   return (
     <>
-      <Navbar expand="lg">
-        <Container class="d-flex justify-content-space-evenly">
-          <Navbar.Brand href="#home">
-            <Logo />
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <Nav.Link href="#home">Witaj</Nav.Link>
-              <Nav.Link href="#about-us">O nas</Nav.Link>
-              <Nav.Link href="#terms">Oferta</Nav.Link>
-              <Nav.Link href="#gallery">Nasze Samochody</Nav.Link>
-              <Nav.Link href="#contact">Kontakt</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <header className="sticky-header">
+        <Navbar expand="lg" className="navbar-dark" aria-label="Header">
+          <Container className="d-flex justify-content-between">
+            <Navbar.Brand href="#home">
+              <Logo />
+            </Navbar.Brand>
+            <Navbar.Toggle as="button" className="navbar-toggler-custom" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link key="home" href="#home">
+                  Witaj
+                </Nav.Link>
+                <Nav.Link key="about-us" href="#about-us">
+                  O nas
+                </Nav.Link>
+                <Nav.Link key="terms" href="#terms">
+                  Oferta
+                </Nav.Link>
+                <Nav.Link key="gallery" href="#gallery">
+                  Nasze Samochody
+                </Nav.Link>
+                <Nav.Link key="contact" href="#contact">
+                  Kontakt
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     </>
   );
 }
