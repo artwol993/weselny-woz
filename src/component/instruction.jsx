0d12d8cac2f8ebc2f7c44d5d2285b0270c310327
Artur <artwol993@gmail.com>
2023-10-04 08:57:35 +0200

    update instruction

--- src/component/instruction.jsx
+++ src/component/instruction.jsx
@@ -1,50 +1,53 @@
 import React from "react";
 import "../style/instruction.css";
+import { Container } from "react-bootstrap";
 
-const Instruction = () => {
+function Instruction() {
   return (
-    <section id="instruction-section">
-      <div className="overlay">
-        <div className="instruction-container">
-          {/* Pierwsza Część */}
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <h2 className="section-heading">Rezerwacja samochodu</h2>
-              <p className="section-subheading">
-                Rezerwacja z nami to prosty i szybki proces. Wszystko sprowadza
-                się do pięciu łatwych kroków. Zachęcamy do skorzystania z
-                naszego formularza rezerwacyjnego!
-              </p>
+    <>
+      <section id="instruction" className="instruction-section">
+        <Container>
+          <div className="instruction-container">
+            {/* Pierwsza Część */}
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading">Rezerwacja samochodu</h2>
+                <p className="section-subheading">
+                  Rezerwacja z nami to prosty i szybki proces. Wszystko
+                  sprowadza się do pięciu łatwych kroków. Zachęcamy do
+                  skorzystania z naszego formularza rezerwacyjnego!
+                </p>
+              </div>
             </div>
-          </div>
 
-          {/* Druga Część */}
-          <div className="steps-row">
-            <div className="step">
-              <div className="step-circle">1</div>
-              Wybierz samochód
-            </div>
-            <div className="step">
-              <div className="step-circle">2</div>
-              Wypełnij formularz
-            </div>
-            <div className="step">
-              <div className="step-circle">3</div>
-              Poczekaj na wycenę
-            </div>
-            <div className="step">
-              <div className="step-circle">4</div>
-              Podpisz umowę
-            </div>
-            <div className="step">
-              <div className="step-circle">5</div>
-              Baw się dobrze!
+            {/* Druga Część */}
+            <div className="steps-row">
+              <div className="step">
+                <div className="step-circle">1</div>
+                Wybierz samochód
+              </div>
+              <div className="step">
+                <div className="step-circle">2</div>
+                Wypełnij formularz
+              </div>
+              <div className="step">
+                <div className="step-circle">3</div>
+                Poczekaj na wycenę
+              </div>
+              <div className="step">
+                <div className="step-circle">4</div>
+                Podpisz umowę
+              </div>
+              <div className="step">
+                <div className="step-circle">5</div>
+                Baw się dobrze!
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </section>
+        </Container>
+      </section>
+    </>
   );
-};
+}
 
 export default Instruction;
