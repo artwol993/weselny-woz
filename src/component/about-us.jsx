24d83e5dcf6f09d2df3edbe2ced9d69f68b6ad23
Artur <artwol993@gmail.com>
2024-05-03 14:27:01 +0200

    add config, update about

--- src/component/about-us.jsx
+++ src/component/about-us.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+
+import { motion } from "framer-motion";
 import "../style/about-us.scss";
 
 function AboutUs() {
+  const containerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, ease: "easeInOut" },
+    },
+  };
+
   return (
     <section id="about-us">
       <div className="container container-about-us">
-        <Container>
+        <motion.div
+          className="about-us-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={containerVariants}
+        >
           <p className="about-us-txt">
             W naszej firmie "Weselny Wóz" jesteśmy przekonani, że droga, która
             prowadzi do uroczystości ślubnej, jest nie mniej ważna niż sam ślub.
@@ -16,6 +32,31 @@ function AboutUs() {
             serc wielu par młodych w okolicach Łukowa (Lubelskie), oraz w takich
             miastach jak Warszawa, Lublin czy Białystok.
           </p>
+        </motion.div>
+        <motion.div
+          className="about-us-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={containerVariants}
+        >
+          <p className="about-us-txt">
+            W naszej firmie "Weselny Wóz" jesteśmy przekonani, że droga, która
+            prowadzi do uroczystości ślubnej, jest nie mniej ważna niż sam ślub.
+            Dlatego już od 2023 roku nie tylko dostarczamy luksusowe środki
+            transportu, ale przede wszystkim kreujemy niezapomniane wspomnienia.
+            Nasza pasja i oddanie temu, co robimy, przyczyniły się do zdobywania
+            serc wielu par młodych w okolicach Łukowa (Lubelskie), oraz w takich
+            miastach jak Warszawa, Lublin czy Białystok.
+          </p>
+        </motion.div>
+        <motion.div
+          className="about-us-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={containerVariants}
+        >
           <p className="about-us-txt">
             Nasza misja to nie tylko zapewnienie transportu - to gwarancja
             wyjątkowości, elegancji oraz niezawodności w każdym calu.
@@ -23,12 +64,20 @@ function AboutUs() {
             oczekiwaniom nawet najbardziej wymagających klientów, dbając o
             każdy, nawet najdrobniejszy detal.
           </p>
+        </motion.div>
+        <motion.div
+          className="about-us-box"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={containerVariants}
+        >
           <p className="about-us-txt">
             Wybierając nas, wybierasz nie tylko luksusowy środek transportu, ale
             przede wszystkim pewność, że każdy moment spędzony z nami stanie się
             wyjątkowym wspomnieniem.
           </p>
-        </Container>
+        </motion.div>
       </div>
     </section>
   );
