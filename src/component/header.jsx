25c773ae35061663a7876a41c5b70d1694fc268c
Artur <artwol993@gmail.com>
2024-05-21 09:20:12 +0200

    update header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
+import Logo from "./logo";
+import SmallLogo from "./small-logo";
 
 function Header() {
   const [isVisible, setIsVisible] = useState(false);
@@ -41,7 +43,7 @@ function Header() {
     <>
       <section className={`header-section ${isVisible ? "is-visible" : ""}`}>
         <div className={`h-container ${isDisplayed ? "displayed" : "hidden"}`}>
-          <div className="h-container">
+          <div className="logo-container">
             <div
               className={`hamburger ${isOpen ? "active" : ""}`}
               onClick={toggleMenu}
@@ -50,88 +52,89 @@ function Header() {
               <div className="hamburger-bar"></div>
               <div className="hamburger-bar"></div>
             </div>
-            {/* Menu mobilne */}
-            <ul className={`mobile-nav ${isOpen ? "active" : ""}`}>
-              <li className="h-c-list">
-                <Link
-                  to="home"
-                  smooth={true}
-                  duration={500}
-                  onClick={toggleMenu}
-                >
-                  Start
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link
-                  to="about-us"
-                  smooth={true}
-                  duration={500}
-                  onClick={toggleMenu}
-                >
-                  O nas
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link
-                  to="terms"
-                  smooth={true}
-                  duration={500}
-                  onClick={toggleMenu}
-                >
-                  Oferta
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link
-                  to="gallery"
-                  smooth={true}
-                  duration={500}
-                  onClick={toggleMenu}
-                >
-                  Nasze Samochody
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link
-                  to="contact"
-                  smooth={true}
-                  duration={500}
-                  onClick={toggleMenu}
-                >
-                  Kontakt
-                </Link>
-              </li>
-            </ul>
-            {/* Menu desktopowe */}
-            <ul className="desktop-nav">
-              <li className="h-c-list">
-                <Link to="home" smooth={true} duration={500}>
-                  Start
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link to="about-us" smooth={true} duration={500}>
-                  O nas
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link to="terms" smooth={true} duration={500}>
-                  Oferta
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link to="gallery" smooth={true} duration={500}>
-                  Nasze Samochody
-                </Link>
-              </li>
-              <li className="h-c-list">
-                <Link to="contact" smooth={true} duration={500}>
-                  Kontakt
-                </Link>
-              </li>
-            </ul>
+            <div className="logo-wrapper">
+              <div className="desktop-logo">
+                <Logo />
+              </div>
+              <div className="mobile-logo">
+                <SmallLogo />
+              </div>
+            </div>
           </div>
+          <ul className={`mobile-nav ${isOpen ? "active" : ""}`}>
+            <li className="h-c-list">
+              <Link to="home" smooth={true} duration={500} onClick={toggleMenu}>
+                Start
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link
+                to="about-us"
+                smooth={true}
+                duration={500}
+                onClick={toggleMenu}
+              >
+                O nas
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link
+                to="terms"
+                smooth={true}
+                duration={500}
+                onClick={toggleMenu}
+              >
+                Oferta
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link
+                to="gallery"
+                smooth={true}
+                duration={500}
+                onClick={toggleMenu}
+              >
+                Nasze Samochody
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link
+                to="contact"
+                smooth={true}
+                duration={500}
+                onClick={toggleMenu}
+              >
+                Kontakt
+              </Link>
+            </li>
+          </ul>
+          <ul className="desktop-nav">
+            <li className="h-c-list">
+              <Link to="home" smooth={true} duration={500}>
+                Start
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link to="about-us" smooth={true} duration={500}>
+                O nas
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link to="terms" smooth={true} duration={500}>
+                Oferta
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link to="gallery" smooth={true} duration={500}>
+                Nasze Samochody
+              </Link>
+            </li>
+            <li className="h-c-list">
+              <Link to="contact" smooth={true} duration={500}>
+                Kontakt
+              </Link>
+            </li>
+          </ul>
         </div>
       </section>
     </>
@@ -139,48 +142,3 @@ function Header() {
 }
 
 export default Header;
-
-/*
-import React from "react";
-import "../style/header.scss";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Logo from "./logo";
-
-function Header() {
-  return (
-    <>
-      <Navbar expand="lg" className="navbar-dark" aria-label="Header">
-        <Container className="d-flex justify-content-between">
-          <Navbar.Brand href="#home">
-            <Logo />
-          </Navbar.Brand>
-          <Navbar.Toggle as="button" className="navbar-toggler-custom" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <Nav.Link key="home" href="#home">
-                Witaj
-              </Nav.Link>
-              <Nav.Link key="about-us" href="#about-us">
-                O nas
-              </Nav.Link>
-              <Nav.Link key="terms" href="#terms">
-                Oferta
-              </Nav.Link>
-              <Nav.Link key="gallery" href="#gallery">
-                Nasze Samochody
-              </Nav.Link>
-              <Nav.Link key="contact" href="#contact">
-                Kontakt
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
-}
-
-export default Header;
-*/
